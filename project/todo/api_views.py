f2af85d6c8bd786dfc2e78edb72876b3341b9e92
michalmytych <michamytych@gmail.com>
2020-09-23 12:20:25 +0200

    #12 change function based views to models viewsets

--- project/todo/api_views.py
+++ project/todo/api_views.py
@@ -1,16 +1,14 @@
 from django.contrib.auth.models import User
 
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework import status, generics
-from rest_framework.response import Response
+from rest_framework import viewsets
 
-from .models import ThingToDo
-from .serializers import ThingToDoSerializer, UserSerializer
+from .models import ThingToDo, ProjectToDo
+from .serializers import ThingToDoSerializer, UserSerializer, ProjectToDoSerializer
 from .permissions import IsOwnerOrReadOnly
 
 
-class UserList(generics.ListAPIView):
+class UserViewSet(viewsets.ModelViewSet):
     """
         Class for viewing User objects queryset.
     """
@@ -18,19 +16,13 @@ class UserList(generics.ListAPIView):
     serializer_class = UserSerializer
     permission_classes = [IsAdminUser]
 
-
-class UserDetail(generics.RetrieveAPIView):
-    """
-        Class for viewing single User object data.
-    """
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-    permission_classes = [IsAdminUser]
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
 
 
-class ToDoList(generics.ListCreateAPIView):
+class ThingToDoViewSet(viewsets.ModelViewSet):
     """
-        Class for viewing ThingToDo objects queryset.
+        Viewset for ProjectToDo - model for collection of ThingToDo objects.
     """
     queryset = ThingToDo.objects.all()
     serializer_class = ThingToDoSerializer
@@ -40,30 +32,13 @@ class ToDoList(generics.ListCreateAPIView):
         serializer.save(user=self.request.user)
 
 
-@api_view(['GET', 'PUT', 'DELETE'])
-@permission_classes([IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly])
-def todo_detail(request, pk):
+class ProjectToDoViewSet(viewsets.ModelViewSet):
     """
-        Function for getting, editing and deleting single ThingToDo object.
+        Viewset for ProjectToDo - model for collection of ThingToDo objects.
     """
-    try:
-        todo = ThingToDo.objects.get(id=pk)
-    except ThingToDo.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+    queryset = ProjectToDo.objects.all()
+    serializer_class = ProjectToDoSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
-    if request.method == 'GET':
-        serializer = ThingToDoSerializer(todo)
-        return Response(serializer.data)
-
-    elif request.method == 'PUT':
-        serializer = ThingToDoSerializer(instance=todo, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    elif request.method == 'DELETE':
-        todo = ThingToDo.objects.get(id=pk)
-        todo.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)

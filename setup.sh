3e9a7746335b2eac8b6dbb00fe622fbd57e2862d
Michał Mytych <michamytych@gmail.com>
2020-11-11 15:07:32 +0100

    change setup script structure & add new functionalities

--- setup.sh
+++ setup.sh
@@ -1,89 +1,144 @@
 #!/bin/bash
 
-echo -e "\e[34m>>>> Starting app setup... \e[0m"
+ERROR_COUNT=0
 
-error_count=0
+# Sets up colors of text
+setup_color() {
+	if [ -t 1 ]; then
+		RED=$(printf '\033[31m')
+		GREEN=$(printf '\033[32m')
+		YELLOW=$(printf '\033[33m')
+		BLUE=$(printf '\033[34m')
+		RESET=$(printf '\033[m')
+	else
+		RED=""
+		GREEN=""
+		YELLOW=""
+		BLUE=""
+		RESET=""
+	fi
+}
 
-# install react and other required npm packages
-cd frontend && npm install
+# Error handler, $1 is success message, $2 is error message
+handle_errors() {
+   if [ $? -eq 0 ]; then
+      echo -e $1
+   else
+      echo -e $2
+      ERROR_COUNT+=1
+   fi
+}
 
-if [ $? -eq 0 ]; then
-   echo -e "\e[32m>>>> Node dependencies installed successfully with npm.\e[0m"
-else
-   echo -e "\e[31m>>>> Error while installing Node dependencies.\e[0m"
-   error_count += 1
-fi
+display_start_message() {
+   echo -e "$BLUE>>>> Starting app setup... <<<<$RESET"
+}
 
-# create python3 virtualenv
-cd ../backend && python3 -m venv venv && source venv/bin/activate
+setup_frontend () {
+   cd frontend && npm install
 
-if [ $? -eq 0 ]; then
-   echo -e "\e[32m>>>> Python3 virtualenv created successfully.\e[0m"
-else
-   echo -e "\e[31m>>>> Error while creating Python3 virtualenv.\e[0m"
-   error_count += 1
-fi
+   handle_errors "$GREEN>>>> Node dependencies installed successfully with npm. <<<<$GREEN" "$RED>>>> Error while installing Node dependencies. <<<<$RESET"
+}
 
-# install required packages with pip3
-pip3 install -r requirements.txt
+setup_python_venv() {
+   cd ../backend && python3 -m venv venv && source venv/bin/activate
 
-if [ $? -eq 0 ]; then
-   echo -e "\e[32m>>>> Python requirements installed successfully with Pip.\e[0m"
-else
-   echo -e "\e[31m>>>> Error while installing requirements from requirements.txt\e[0m"
-   error_count += 1
-fi
+   handle_errors "$GREEN>>>> Python3 virtualenv created successfully. <<<<$RESET" "$RED>>>> Error while creating Python3 virtualenv. <<<<$RESET"
+}
 
-# generate SECRET_KEY
-cd backend/ && python3 keygen.py
+install_requirements() {
+   pip3 install -r requirements.txt
 
-if [ $? -eq 0 ]; then
-   echo -e "\e[32m>>>> Django SECRET_KEY generated successfully.\e[0m"
-else
-   echo -e "\e[31m>>>> Error while generating Django SECRET_KEY\e[0m"
-   error_count += 1
-fi
+   handle_errors "$GREEN>>>> Python requirements installed successfully with Pip. <<<<$RESET" "$RED>>>> Error while installing requirements from requirements.txt <<<<$RESET"
+}
 
-cd ../../ && echo "secrets.py" >> .gitignore
+generate_django_secret_key() {
+   cd backend/ && python3 keygen.py
 
+   handle_errors "$GREEN>>>> Django SECRET_KEY generated successfully. <<<<$RESET" "$RED>>>> Error while generating Django SECRET_KEY <<<<$RESET"
 
-if [ $? -eq 0 ]; then
-   echo -e "\e[32m>>>> Django SECRET_KEY added to git-ignored files.\e[0m"
-else
-   echo -e "\e[31m>>>> Error while adding SECRET_KEY to git-ignored files.\e[0m"
-   error_count += 1
-fi
+   cd ../../ && echo "secrets.py" >> .gitignore
+   
+   handle_errors "$GREEN>>>> Django SECRET_KEY added to git-ignored files. <<<<$RESET" "$RED>>>> Error while adding SECRET_KEY to git-ignored files. <<<<$RESET"
+}
 
-# setup django app:
-cd backend && python3 manage.py makemigrations && python3 manage.py migrate
+setup_database() {
+   cd backend && python3 manage.py makemigrations && python3 manage.py migrate
 
-if [ $? -eq 0 ]; then
-   echo -e "\e[32m>>>> Database constructed, migrations applied.\e[0m"
-else
-   echo -e "\e[31m>>>> Error while constructing database.\e[0m"
-   error_count += 1
-fi
+   handle_errors "$GREEN>>>> Database constructed, migrations applied. <<<<$RESET" "$RED>>>> Error while constructing database. <<<<$RESET"
+}
 
-# optionally fill database with sample data:
-echo -e "\e[34m>>>> Do you want to fill database with sample data?\e[0m"
-echo ""
-echo -e "\e[34m>>>> Type 1 or 2 and hit ENTER.\e[0m"
-select yn in "Yes" "No"; do
-    case $yn in
-        Yes ) python3 manage.py loaddata data.json; break;;
-        No ) echo -e "\e[36m>>>> Filling database with fixtures skipped.\e[0m"; break;;
-    esac
-done
+load_fixtures() {
+   python3 manage.py loaddata data.json
 
-if [ $? -eq 0 ]; then
-    echo -e "\e[32m >>>> Database filled with sample data.\e[0m"
-else
-    echo -e "\e[31m>>>> Error while filling database with sample data.\e[0m"
-    error_count += 1
-fi
+   handle_errors "$GREEN >>>> Database filled with sample data. <<<<$RESET" "$RED>>>> Error while filling database with sample data. <<<<$RESET"
+}
 
-if [ $error_count -eq 0 ]; then
-   echo -e "\e[32m>>>> *** Setup done. ***\e[0m"
-else
-   echo -e "\e[31m>>>> Setup failed. $error_count errors occured.\e[0m"
-fi
+fill_database_if_wanted() {
+   echo -e "$BLUE>>>> Do you want to fill database with sample data? <<<<$RESET"
+   echo ""
+   echo -e "$BLUE>>>> Type 1 or 2 and hit ENTER. <<<<$RESET"
+   select yn in "Yes" "No"; do
+      case $yn in
+         Yes ) load_fixtures; break;;
+         No ) echo -e "$YELLOW>>>> Filling database with fixtures skipped. <<<<$RESET"; break;;
+      esac
+   done
+}
+
+check_if_setup_succedeed() {
+   if [ $ERROR_COUNT -eq 0 ]; then
+      echo -e "$GREEN>>>> *** Setup done. *** <<<< $RESET"
+   else
+      echo -e "$RED>>>> Setup failed. $ERROR_COUNT errors occured. <<<<$RESET"
+   fi
+}
+
+create_superuser_if_wanted() {
+   echo -e "$BLUE>>>> Do you want to create superuser (admin user) now? <<<<$RESET"
+   echo ""
+   echo -e "$BLUE>>>> Type 1 or 2 and hit ENTER. <<<<$RESET"
+   select yn in "Yes" "No"; do
+      case $yn in
+         Yes ) python3 manage.py createsuperuser; break;;
+         No ) echo -e "$YELLOW>>>> Creating superuser skipped. <<<<$RESET"; break;;
+      esac
+   done
+}
+
+run_django_app() {
+   echo -e "$BLUE>>>> Launching Django backend app... <<<<$RESET"
+
+   cd ..
+   source backend/venv/bin/activate && python3 backend/manage.py runserver
+
+   handle_errors "$GREEN $BOLD >>>> Thanks for trying out this app :) <<<< $RESET" "$RED>>>> Error while running backend Django app. <<<<$RESET"
+}
+
+run_app_if_wanted() {
+   echo -e "$BLUE>>>> Do you want to run Django API on port 8000 now? <<<<$RESET"
+   echo ""
+   echo -e "$BLUE>>>> Type 1 or 2 and hit ENTER. <<<<$RESET"
+   select yn in "Yes" "No"; do
+      case $yn in
+         Yes ) run_django_app; break;;
+         No ) echo -e "$YELLOW>>>> Running Django API skipped. <<<<$RESET"; break;;
+      esac
+   done
+}
+
+setup_app() {
+   setup_color
+   display_start_message
+   setup_frontend
+   setup_python_venv
+   install_requirements
+   generate_django_secret_key
+   setup_database
+   fill_database_if_wanted
+   create_superuser_if_wanted
+   check_if_setup_succedeed
+}
+
+setup_app
+
+run_app_if_wanted
